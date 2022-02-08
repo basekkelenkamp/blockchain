2fc1e9acf8e5a66a805b749bccd8e1e88b3fdc5f
Bas Ekkelenkamp <basekkelenkamp@MBP-van-Bas.fritz.box>
2022-02-08 22:03:48 +0100

    fix hasher

--- hasher.py
+++ hasher.py
@@ -1,6 +1,11 @@
+from hashlib import sha256
+
+
 def hash_mod10sha(input):
-    no_space = input.replace(' ', '')
-    ascii_values = [character if character.isdigit() else ord(character) for character in no_space]
+    no_space = input.replace(" ", "")
+    ascii_values = [
+        character if character.isdigit() else ord(character) for character in no_space
+    ]
 
     combined_values = []
     for value in ascii_values:
@@ -9,15 +14,16 @@ def hash_mod10sha(input):
 
     chunk_values = split_into_chunks(combined_values, 10)
     filled_values = add_to_last_element(chunk_values)
+    final_values = merge_lists(filled_values)
 
-    breakpoint()
-    return input
+    string_value = "".join([str(value) for value in final_values])
+    return sha256(string_value.encode("utf-8")).hexdigest()
 
 
 def split_into_chunks(li: list, chunk: int):
     if len(li) < chunk:
         return li
-    return [li[i:i+chunk] for i in range(0, len(li), chunk)]
+    return [li[i : i + chunk] for i in range(0, len(li), chunk)]
 
 
 def add_to_last_element(input: list):
@@ -26,3 +32,17 @@ def add_to_last_element(input: list):
 
     input[-1].extend(li[:number_to_add])
     return input
+
+
+def merge_lists(input: list):
+    if len(input) == 1:
+        return input[0]
+
+    merged_list = [
+        (input[0][index] + input[1][index]) % 10 for index, li in enumerate(input[0])
+    ]
+
+    del input[:2]
+    input.insert(0, merged_list)
+
+    return merge_lists(input)
--- util/util.py
+++
@@ -1,2 +0,0 @@
-def merge_strings(a: str, timestamp: str, b: str):
-    return f"{a}{timestamp}{b}"
